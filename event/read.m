8758acc0d703cdb8466f238a4b8e2babd25d1be9
hideaki-k <h.kinoshita@kimura-lab.net>
2020-09-26 01:43:46 +0900

    正規化相互相関inMATLAB

--- event/read.m
+++ event/read.m
@@ -1,2 +1,6 @@
-filename = "dvSave-2020_09_22_18_29_15.mat";
+filename ='dvSave-2020_09_22_18_29_15.mat';
 s = load(filename);
+X = s.aedat.data.polarity.x
+Y = s.aedat.data.polarity.y
+T = s.aedat.data.polarity.timeStamp
+plot3(T,X,Y,'.','MarkerSize',0.1)
