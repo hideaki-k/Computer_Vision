62ed5a4a715ea93e491fbc5f8b3b1b09927b8eb3
hideaki-k <h.kinoshita@kimura-lab.net>
2020-12-08 12:06:05 +0900

    semaseg > sample

---
+++ matlab/semantic_segmentation/sample.m
@@ -0,0 +1,130 @@
+resnet18();
+pretrainedURL = 'https://www.mathworks.com/supportfiles/vision/data/deeplabv3plusResnet18CamVid.mat';
+pretrainedFolder = fullfile(tempdir,'pretrainedNetwork');
+pretrainedNetwork = fullfile(pretrainedFolder,'deeplabv3plusResnet18CamVid.mat'); 
+if ~exist(pretrainedNetwork,'file')
+    mkdir(pretrainedFolder);
+    disp('Downloading pretrained network (58 MB)...');
+    websave(pretrainedNetwork,pretrainedURL);
+end
+imageURL = 'http://web4.cs.ucl.ac.uk/staff/g.brostow/MotionSegRecData/files/701_StillsRaw_full.zip';
+labelURL = 'http://web4.cs.ucl.ac.uk/staff/g.brostow/MotionSegRecData/data/LabeledApproved_full.zip';
+
+outputFolder = fullfile(tempdir,'CamVid'); 
+labelsZip = fullfile(outputFolder,'labels.zip');
+imagesZip = fullfile(outputFolder,'images.zip');
+
+if ~exist(labelsZip, 'file') || ~exist(imagesZip,'file')   
+    mkdir(outputFolder)
+       
+    disp('Downloading 16 MB CamVid dataset labels...'); 
+    websave(labelsZip, labelURL);
+    unzip(labelsZip, fullfile(outputFolder,'labels'));
+    
+    disp('Downloading 557 MB CamVid dataset images...');  
+    websave(imagesZip, imageURL);       
+    unzip(imagesZip, fullfile(outputFolder,'images'));    
+end
+
+imgDir = fullfile(outputFolder,'images','701_StillsRaw_full');
+imds = imageDatastore(imgDir);
+I = readimage(imds,1);
+I = histeq(I);
+imshow(I)
+
+classes = [
+    "Sky"
+    "Building"
+    "Pole"
+    "Road"
+    "Pavement"
+    "Tree"
+    "SignSymbol"
+    "Fence"
+    "Car"
+    "Pedestrian"
+    "Bicyclist"
+    ];
+
+labelIDs = camvidPixelLabelIDs();
+labelDir = fullfile(outputFolder,'labels');
+pxds = pixelLabelDatastore(labelDir,classes,labelIDs);
+
+C = readimage(pxds,1);
+cmap = camvidColorMap;
+B = labeloverlay(I,C,'ColorMap',cmap);
+imshow(B)
+pixelLabelColorbar(cmap,classes);
+tbl = countEachLabel(pxds)
+frequency = tbl.PixelCount/sum(tbl.PixelCount);
+
+bar(1:numel(classes),frequency)
+xticks(1:numel(classes)) 
+xticklabels(tbl.Name)
+xtickangle(45)
+ylabel('Frequency')
+
+[imdsTrain, imdsVal, imdsTest, pxdsTrain, pxdsVal, pxdsTest] = partitionCamVidData(imds,pxds);
+numTrainingImages = numel(imdsTrain.Files)
+numValImages = numel(imdsVal.Files)
+numTestingImages = numel(imdsTest.Files)
+% Specify the network image size. This is typically the same as the traing image sizes.
+imageSize = [720 960 3];
+
+% Specify the number of classes.
+numClasses = numel(classes);
+
+% Create DeepLab v3+.
+lgraph = deeplabv3plusLayers(imageSize, numClasses, "resnet18");
+imageFreq = tbl.PixelCount ./ tbl.ImagePixelCount;
+classWeights = median(imageFreq) ./ imageFreq
+
+pxLayer = pixelClassificationLayer('Name','labels','Classes',tbl.Name,'ClassWeights',classWeights);
+lgraph = replaceLayer(lgraph,"classification",pxLayer);
+% Define validation data.
+pximdsVal = pixelLabelImageDatastore(imdsVal,pxdsVal);
+
+% Define training options. 
+options = trainingOptions('sgdm', ...
+    'LearnRateSchedule','piecewise',...
+    'LearnRateDropPeriod',10,...
+    'LearnRateDropFactor',0.3,...
+    'Momentum',0.9, ...
+    'InitialLearnRate',1e-3, ...
+    'L2Regularization',0.005, ...
+    'ValidationData',pximdsVal,...
+    'MaxEpochs',30, ...  
+    'MiniBatchSize',2, ...
+    'Shuffle','every-epoch', ...
+    'CheckpointPath', tempdir, ...
+    'VerboseFrequency',2,...
+    'Plots','training-progress',...
+    'ValidationPatience', 4);
+
+augmenter = imageDataAugmenter('RandXReflection',true,...
+    'RandXTranslation',[-10 10],'RandYTranslation',[-10 10]);
+
+pximds = pixelLabelImageDatastore(imdsTrain,pxdsTrain, ...
+    'DataAugmentation',augmenter);
+doTraining = true;
+if doTraining    
+    [net, info] = trainNetwork(pximds,lgraph,options);
+else
+    data = load(pretrainedNetwork); 
+    net = data.net;
+end
+
+I = readimage(imdsTest,35);
+C = semanticseg(I, net);
+
+B = labeloverlay(I,C,'Colormap',cmap,'Transparency',0.4);
+imshow(B)
+
+pixelLabelColorbar(cmap, classes);
+expectedResult = readimage(pxdsTest,35);
+actual = uint8(C);
+expected = uint8(expectedResult);
+imshowpair(actual, expected)
+
+iou = jaccard(C,expectedResult);
+table(classes,iou)
